bba95fb4cae1d745a23f1284755909cbf6d60e7f
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-23 15:47:19 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Movie_title = 1
+    basic.showString("Da Vinci Code")
+    basic.pause(10380000)
+    Movie_title = 0
+})
+let Movie_title = 0
+let Tom_Hanks = "Tom Hanks"
 basic.forever(function () {
-	
+    if (Movie_title == 1) {
+        Tom_Hanks = "Robert Langdon"
+    } else {
+        Tom_Hanks = "Tom Hanks"
+    }
 })
